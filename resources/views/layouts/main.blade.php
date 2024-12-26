becf7f7acc3cc04432bc28efd8b3b8d1b578c569
Rena <ren484@mail.ru>
2024-12-26 14:27:13 +0400

    edit_main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @yield('content')
